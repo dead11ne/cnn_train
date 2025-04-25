d16bd8e3e30dea44d6e47bdc2f9eb419638f73a9
Yaroslav Baev <gamercity123456@gmail.com>
2025-04-26 02:11:09 +0300

    ädd CustomDataset

--- cnn.py
+++ cnn.py
@@ -9,6 +9,8 @@ from torchvision.transforms import transforms
 import matplotlib.pyplot as plt
 from PIL import Image
 
+path = 'C:/Users/MSI/Desktop/datasets/Rice_Image_Dataset'
+
 train_transform = transforms.Compose([
     transforms.CenterCrop(225),
     transforms.RandomHorizontalFlip(p=0.45),
@@ -21,6 +23,47 @@ val_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
+train_dataload = DataLoader(
+    dataset=path,
+    batch_size=128,
+    shuffle=True
+    )
+
+val_dataload = DataLoader(
+    dataset=path,
+    batch_size=128,
+    shuffle=True
+    )
+
+class CustomDataset(Dataset):
+    def __init__(self, path, transform=None):
+        self.path = path
+        self.transform = transform
+        self.classes = os.listdir(path)
+        self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
+        self.images = self._load_images()
+
+    def _load_images(self):
+        images = []
+        for class_name in self.classes:
+            class_dir = os.path.join(self.path, class_name)
+        for image_name in os.listdir(class_dir):
+            image_path = os.path.join(class_dir, image_name)
+            images.append((image_path, self.class_to_idx[class_name]))
+        return images           
+
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        image_path, label = self.images[index]
+        image = Image.open(image_path).convert('RGB')
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        return image, label
 
 class MyNet(nn.Module):
     def __init__(self):
